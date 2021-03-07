e5330a2b223f05e4d90fb160d097d1f080d691ca
whyroland <42717361+whyroland@users.noreply.github.com>
2021-03-07 06:43:43 -0500

    oasdoasodoad
    oh my

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -8,10 +8,19 @@ const int RED_LED = 2; // Red Light - Signals when it is invalid to get food
 const int RELAY = 8; // Dispenser Relay
 const int BUTTON = 3; // Button
 const int BUZZER = 5; // Music player
-const int COOLDOWN = 500;
+const bool IS_AUTOMATIC = false;
+
+// THIS IS THE COOLDOWN
+// THIS IS THE COOLDOWN
+// THIS IS THE COOLDOWN
+const unsigned long COOLDOWN = 5000; // Cooldown time to reset to in MILLISECONDS 
+// THIS IS THE COOLDOWN
+// THIS IS THE COOLDOWN
+// THIS IS THE COOLDOWN
 
 int buttonState = 0; // Whether the button is pressed or not
-int cooldownLeft = 0;
+unsigned long endTime = 0; // Time at the end of the cooldown
+bool isInCooldown = false;
 
 void setup() {
   // Setup the input and outputs correctly
@@ -33,20 +42,56 @@ void loop() {
   
   if (buttonState == HIGH) {
     if (digitalRead(GREEN_LED) == HIGH) { // Valid to press, make sure the Corgi does not become overweight by eating too much!!!
-      digitalWrite(YELLOW_LED, HIGH);
-      digitalWrite(RELAY, LOW);
-      digitalWrite(GREEN_LED, LOW);
-      delay(1250);
-      digitalWrite(RELAY, HIGH);
-    } else { // Not valid to press, must wait for the cooldown time
-      digitalWrite(YELLOW_LED, LOW);
-      digitalWrite(RED_LED, HIGH);
-      tone(BUZZER, 1000);
-      delay(750);
-      noTone(BUZZER);
-      digitalWrite(YELLOW_LED, HIGH);
-      digitalWrite(RED_LED, LOW);
+      dispense();
+    } 
+    else { // Not valid to press, must wait for the cooldown time
+      error();
     }
   }
-  
+  else if (isInCooldown && millis() > endTime) { // Checks the cooldown
+      digitalWrite(YELLOW_LED, LOW);
+      digitalWrite(GREEN_LED, HIGH);
+      isInCooldown = false; // Only have to care about whether in cooldown or not at this point, no need to change endTime
+      if (IS_AUTOMATIC) {
+        for (int i = 0; i < 5; i++) {// Blink for 5 seconds before redispensing
+          digitalWrite(GREEN_LED, LOW);
+          tone(BUZZER, 500);
+          fakeDelay(500);
+          noTone(BUZZER);
+          digitalWrite(GREEN_LED, HIGH);
+          fakeDelay(500);
+        }
+        dispense();
+      }
+  }
+
+}
+
+void fakeDelay(int time) {
+  unsigned long tempEndTime = millis() + time;
+  while (tempEndTime - millis() > (unsigned long) 0) {
+    if (millis() > tempEndTime) // safeguard incase it spazzes out
+      break;
+  }
+}
+
+void dispense() {
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RELAY, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  fakeDelay(1500);
+  digitalWrite(RELAY, HIGH);
+  // Setup timer
+  endTime = millis() + COOLDOWN; // Does not account for the 50 day overflow
+  isInCooldown = true;
+}
+
+void error() {
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, HIGH);
+  tone(BUZZER, 1000);
+  fakeDelay(750);
+  noTone(BUZZER);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(RED_LED, LOW);
 }
